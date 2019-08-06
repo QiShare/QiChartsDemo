021e024039d5fb35fc047cebd2417d966fe8cb0d
wangdacheng <>
2019-08-06 14:12:20 +0800

    创建工程
    QiChartsDemo + 内嵌target QiCharts

---
+++ QiCharts/QiCharts.h
@@ -0,0 +1,19 @@
+//
+//  QiCharts.h
+//  QiCharts
+//
+//  Created by wangdacheng on 2019/8/6.
+//  Copyright © 2019 qishare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QiCharts.
+FOUNDATION_EXPORT double QiChartsVersionNumber;
+
+//! Project version string for QiCharts.
+FOUNDATION_EXPORT const unsigned char QiChartsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QiCharts/PublicHeader.h>
+
+
---
+++ QiChartsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QiChartsDemo
+//
+//  Created by wangdacheng on 2019/8/6.
+//  Copyright © 2019 qishare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
