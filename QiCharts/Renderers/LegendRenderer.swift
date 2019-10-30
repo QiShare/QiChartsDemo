da8e3c1285a0d639fa3240d483dcd68fb35696b3
wangdacheng <wangdacheng@360.cn>
2019-10-30 14:46:08 +0800

    删除combine相关
    删除combine相关代码

--- QiCharts/Renderers/LegendRenderer.swift
+++ QiCharts/Renderers/LegendRenderer.swift
@@ -43,7 +43,7 @@ open class LegendRenderer: Renderer
             {
                 guard let dataSet = data.getDataSetByIndex(i) else { continue }
                 
-                var clrs: [NSUIColor] = dataSet.colors
+                let clrs: [NSUIColor] = dataSet.colors
                 let entryCount = dataSet.entryCount
                 
                 // if we have a barchart with stacked bars
@@ -51,7 +51,7 @@ open class LegendRenderer: Renderer
                     (dataSet as! IBarChartDataSet).isStacked
                 {
                     let bds = dataSet as! IBarChartDataSet
-                    var sLabels = bds.stackLabels
+                    let sLabels = bds.stackLabels
                     
                     for j in 0..<min(clrs.count, bds.stackSize)
                     {
@@ -203,7 +203,7 @@ open class LegendRenderer: Renderer
         let labelLineHeight = labelFont.lineHeight
         let formYOffset = labelLineHeight / 2.0
 
-        var entries = legend.entries
+        let entries = legend.entries
         
         let defaultFormSize = legend.formSize
         let formToTextSpace = legend.formToTextSpace
@@ -291,9 +291,9 @@ open class LegendRenderer: Renderer
         {
         case .horizontal:
             
-            var calculatedLineSizes = legend.calculatedLineSizes
-            var calculatedLabelSizes = legend.calculatedLabelSizes
-            var calculatedLabelBreakPoints = legend.calculatedLabelBreakPoints
+            let calculatedLineSizes = legend.calculatedLineSizes
+            let calculatedLabelSizes = legend.calculatedLabelSizes
+            let calculatedLabelBreakPoints = legend.calculatedLabelBreakPoints
             
             var posX: CGFloat = originPosX
             var posY: CGFloat
